1a93d28ffa72e7fd6ad8b4251e9f53f61e8117b5
WuChenDi <wuchendi96@gmail.com>
2022-08-06 05:02:05 +0000

    Deploying to gh-pages from @ HJFront/next-mdx-template@7d6b607d9beb4f9a2c639bac04bc6693c8683969 🚀

--- _next/static/dUoUvyJ8IVfpElD7ul7sy/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/ae0e3e027412e072.css","static/chunks/pages/index-e2b5fa79ff5107a8.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],"/page":["static/chunks/pages/page-a928769cacb71fce.js"],sortedPages:["/","/_app","/_error","/page"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
